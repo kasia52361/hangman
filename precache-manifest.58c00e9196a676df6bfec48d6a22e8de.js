b629af2761b4c22ee0d8de2211c223900e1193fe
kasiapalka <katarzyna.palka@quantoo.pl>
2019-12-01 21:57:58 +0100

    Updates

---
+++ precache-manifest.58c00e9196a676df6bfec48d6a22e8de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd106f18e02c5229ee8859c3d366d095",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "ce969af7f302b6c237ef",
+    "url": "/hangman/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ce41c1e1f20b92401ecb",
+    "url": "/hangman/static/css/main.10fe2b6d.chunk.css"
+  },
+  {
+    "revision": "ce969af7f302b6c237ef",
+    "url": "/hangman/static/js/2.51370224.chunk.js"
+  },
+  {
+    "revision": "ce41c1e1f20b92401ecb",
+    "url": "/hangman/static/js/main.6b933113.chunk.js"
+  },
+  {
+    "revision": "aa07fa6c4b1aa59d521c",
+    "url": "/hangman/static/js/runtime-main.3a2bf788.js"
+  },
+  {
+    "revision": "61de1d1628915a8e6f6789f1c83f640c",
+    "url": "/hangman/static/media/kitty-body.61de1d16.png"
+  },
+  {
+    "revision": "ff0765938e0ccb431905e0492bb57f05",
+    "url": "/hangman/static/media/kitty-hand-left.ff076593.png"
+  },
+  {
+    "revision": "bc0214b8a37ebc9f4f23ac02333d3ac3",
+    "url": "/hangman/static/media/kitty-hand-right.bc0214b8.png"
+  },
+  {
+    "revision": "3369f4d8444c858b8e73bd120bb997ce",
+    "url": "/hangman/static/media/kitty-head.3369f4d8.png"
+  },
+  {
+    "revision": "3721bd292c8ae480cf7f70a0c94fc6ba",
+    "url": "/hangman/static/media/kitty-leg-left.3721bd29.png"
+  },
+  {
+    "revision": "8cd9e1f35633fd55f97cb1433646bd45",
+    "url": "/hangman/static/media/kitty-leg-right.8cd9e1f3.png"
+  }
+]);
